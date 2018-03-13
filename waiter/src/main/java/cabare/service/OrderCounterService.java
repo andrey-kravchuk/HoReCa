3051227d24fc8db3cb699d915a6f5e933d30b994
GreySlon <garanenkosergii@gmail.com>
2018-03-14 01:37:47 +0200

    added transaction for order counter

--- waiter/src/main/java/cabare/service/OrderCounterService.java
+++ waiter/src/main/java/cabare/service/OrderCounterService.java
@@ -6,6 +6,7 @@ import cabare.repository.OrderCounterRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderCounterService {
@@ -14,10 +15,11 @@ public class OrderCounterService {
   @Autowired
   private OrderCounterRepository orderCounterRepository;
 
+  @Transactional
   public Long nextOrderNumber() {
     if (orderCounter == null) {
       orderCounter = orderCounterRepository.findById(1L)
-          .orElseGet(() -> orderCounterRepository.save(new OrderCounter()));
+          .orElseGet(() -> new OrderCounter());
     }
     return orderCounterRepository.save(orderCounter.next()).getCounter();
   }
