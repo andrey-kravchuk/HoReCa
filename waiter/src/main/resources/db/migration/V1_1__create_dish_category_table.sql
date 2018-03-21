5bfe7649c41da1610d35ac526a0361eed45be251
albaderon <kravchukodessa@gmail.com>
2018-03-21 18:48:13 +0200

    new src

--- waiter/src/main/resources/db/migration/V1_1__create_dish_category_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dish_category (
-  id    BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name  VARCHAR(255) NOT NULL,
-  photo VARCHAR(255),
-  UNIQUE (name)
-)
--- waiter/src/main/resources/db/migration/V1_3__create_employee_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE employee (
-  id      BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name    VARCHAR(255)           NOT NULL,
-  enabled BIT DEFAULT b'1'       NOT NULL
-)
