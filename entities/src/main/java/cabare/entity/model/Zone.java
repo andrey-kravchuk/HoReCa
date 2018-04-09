034a0c76652475eb5e469263bb3dc60c7daaec66
andrey-kravchuk <kravchukodessa@gmail.com>
2018-04-09 10:22:34 +0300

    T19E class Zone

---
+++ entities/src/main/java/cabare/entity/model/Zone.java
@@ -0,0 +1,46 @@
+package cabare.entity.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "zone")
+public class Zone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof Zone)) return false;
+        Zone zone = (Zone) o;
+        return Objects.equals(name, zone.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
+    }
+}
