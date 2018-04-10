4447e75f7c33213cd3b153ccf0c13462199884c2
Alex <aleksrmalyshev@gmail.com>
2018-04-10 22:30:22 +0300

    T21D CabareRepository

--- waiter/src/main/resources/db/migration/V1_1__create_dish_category_table.sql
+++ waiter/src/main/resources/db/migration/V1_1__create_dish_category_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE cabare (
   id    BIGINT PRIMARY KEY AUTO_INCREMENT,
   name  VARCHAR(255) NOT NULL,
   UNIQUE (name)
-)
+);
 
 CREATE TABLE dish_category (
   id    BIGINT PRIMARY KEY AUTO_INCREMENT,
