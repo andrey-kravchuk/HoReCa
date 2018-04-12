04ca9edb98bb67d8406ddf2ef1ebedefeadec244
andrey-kravchuk <kravchukodessa@gmail.com>
2018-04-12 20:01:09 +0300

    DishCategory add getAll()

--- data/src/main/java/cabare/data/DishCategoryRepository.java
+++ data/src/main/java/cabare/data/DishCategoryRepository.java
@@ -2,8 +2,10 @@ package cabare.data;
 
 import cabare.entity.model.DishCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +13,8 @@ public interface DishCategoryRepository extends JpaRepository<DishCategory, Long
     Optional<DishCategory> findById(Long dishCategoryId);
 
     Optional<DishCategory> findByName(String dishCategoryName);
+
+    @Query("select d from DishCategory d")
+    List<DishCategory> getAll();
 
 }
