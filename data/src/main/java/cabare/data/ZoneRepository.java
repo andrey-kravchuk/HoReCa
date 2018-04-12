b96f4b0262a400225188e9960f986f23559c2821
andrey-kravchuk <kravchukodessa@gmail.com>
2018-04-12 20:06:02 +0300

    ZoneRepository add getAll()

--- data/src/main/java/cabare/data/ZoneRepository.java
+++ data/src/main/java/cabare/data/ZoneRepository.java
@@ -2,11 +2,17 @@ package cabare.data;
 
 import cabare.entity.model.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
     Optional<Zone> findById(Long zoneId);
+
+    @Query("select z from Zone z")
+    List<Zone> getAll();
+
 }
