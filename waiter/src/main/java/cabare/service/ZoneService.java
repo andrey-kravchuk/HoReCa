bf8ac1cae1ab8e24b0b9972214f4e7b095fdd95a
andrey-kravchuk <kravchukodessa@gmail.com>
2018-04-12 20:24:53 +0300

    ZoneService

---
+++ waiter/src/main/java/cabare/service/ZoneService.java
@@ -0,0 +1,12 @@
+package cabare.service;
+
+import cabare.entity.model.Zone;
+
+import java.util.List;
+
+public interface ZoneService {
+
+    Zone findById(Long zoneId);
+
+    List<ZoneDto> getZones();
+}
