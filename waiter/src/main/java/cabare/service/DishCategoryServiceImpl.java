e579cc1d36773d5fd8903e9e4b9497398aa304dc
andrey-kravchuk <kravchukodessa@gmail.com>
2018-04-15 17:33:08 +0300

    zone service implement

--- waiter/src/main/java/cabare/service/DishCategoryServiceImpl.java
+++ waiter/src/main/java/cabare/service/DishCategoryServiceImpl.java
@@ -1,20 +1,35 @@
 package cabare.service;
 
+import cabare.data.DishCategoryRepository;
 import cabare.dto.DishCategoryDto;
+import cabare.entity.model.DishCategory;
+import cabare.exception.DishCategoryNotSpecifiedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DishCategoryServiceImpl implements DishCategoryService {
 
-  @Override
-  public List<DishCategoryDto> getDishCategories() {
-    return null;
-  }
+    @Autowired
+    private DishCategoryRepository dishCategoryRepository;
 
-  @Override
-  public DishCategoryDto findById(Long dishCategoryId) {
-    return null;
-  }
+
+    @Override
+    public List<DishCategoryDto> getDishCategories() {
+        return dishCategoryRepository.getAll().stream()
+                .map(dishCategory -> new DishCategoryDto(dishCategory))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public DishCategoryDto findById(Long dishCategoryId) {
+        if (dishCategoryId == null) {
+            throw new DishCategoryNotSpecifiedException();
+        }
+        DishCategory dishCategory = dishCategoryRepository.findById(dishCategoryId).orElseThrow(() -> new DishCategoryNotSpecifiedException());
+        return new DishCategoryDto(dishCategory);
+    }
 }
