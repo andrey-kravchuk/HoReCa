3a479a2f3441a3fc5183b4ec175ef6e6d1b0be63
andrey-kravchuk <kravchukodessa@gmail.com>
2018-04-15 15:14:51 +0300

    zone not found exception class

---
+++ waiter/src/main/java/cabare/exception/ZoneNotFoundException.java
@@ -0,0 +1,13 @@
+package cabare.exception;
+
+public class ZoneNotFoundException extends ApplicationException {
+    private static String msg = "zone is not found";
+
+    public ZoneNotFoundException(){
+        super(msg);
+    }
+
+    public ZoneNotFoundException(String message){
+        super(msg + "; " + message);
+    }
+}
