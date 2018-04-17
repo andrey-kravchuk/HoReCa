6ebdd95ad15fdac6813e1cb8b4f9f49bb0c6afd8
Alex <aleksrmalyshev@gmail.com>
2018-04-17 20:31:19 +0300

    T15W-validation v2

--- waiter/src/main/java/cabare/controller/DishController.java
+++ waiter/src/main/java/cabare/controller/DishController.java
@@ -4,13 +4,17 @@ import cabare.dto.DishDto;
 import cabare.service.DishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/waiter/dish")
 public class DishController {
@@ -19,8 +23,9 @@ public class DishController {
     private DishService dishService;
 
     @RequestMapping(value = "/by_category")
-    public List<DishDto> getDishes(@Valid
-                                   @RequestParam(name = "dish_category_id") Long dishCategoryId
+    public List<DishDto> getDishes(@NotNull(message = "category_id should be specified")
+                                   @Min(value = 1, message = "category_id cannot be less than 1")
+                                   @RequestParam(name = "category_id") Long dishCategoryId
             , Pageable pageable) {
         return dishService.getDishesByCategory(dishCategoryId, pageable);
     }
