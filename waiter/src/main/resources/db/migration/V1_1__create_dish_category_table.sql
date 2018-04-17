b0f11744729eba9a5ac7d3f73b6d83cb6cf89f8f
GreySlon <garanenkosergii@gmail.com>
2018-04-17 15:51:46 +0300

    add cabare id to dish category

--- waiter/src/main/resources/db/migration/V1_1__create_dish_category_table.sql
+++ waiter/src/main/resources/db/migration/V1_1__create_dish_category_table.sql
@@ -14,10 +14,13 @@ CREATE TABLE zone (
 );
 
 CREATE TABLE dish_category (
-  id      BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name    VARCHAR(255) NOT NULL,
-  photo   VARCHAR(255),
-  zone_id BIGINT       NOT NULL,
+  id        BIGINT PRIMARY KEY AUTO_INCREMENT,
+  name      VARCHAR(255) NOT NULL,
+  photo     VARCHAR(255),
+  zone_id   BIGINT       NOT NULL,
+  cabare_id BIGINT       NOT NULL,
+  CONSTRAINT fk_dishcategory_cabare
+  FOREIGN KEY (cabare_id) REFERENCES cabare (id),
   UNIQUE (name),
   CONSTRAINT fk_dishcategory_zone
   FOREIGN KEY (zone_id) REFERENCES zone (id)
--- waiter/src/main/resources/db/migration/V1_8__filling_tables.sql
+++ waiter/src/main/resources/db/migration/V1_8__filling_tables.sql
@@ -8,12 +8,16 @@ INSERT INTO zone (id, name, cabare_id) VALUES ('1', 'Кухня', 1);
 INSERT INTO zone (id, name, cabare_id) VALUES ('2', 'Бар', 1);
 
 # dish categories
-INSERT INTO dish_category (id, name, photo, zone_id) VALUES ('1', 'Закуски', 'snacks.jpg', '1');
-INSERT INTO dish_category (id, name, photo, zone_id)
-VALUES ('2', 'Основные блюда', 'main.jpg', '1');
-INSERT INTO dish_category (id, name, photo, zone_id) VALUES ('3', 'Салаты', 'salads.jpg', '1');
-INSERT INTO dish_category (id, name, photo, zone_id) VALUES ('4', 'Напитки', 'beverages.jpg', '2');
-INSERT INTO dish_category (id, name, photo, zone_id) VALUES ('5', 'Десерты', 'deserts.jpg', '1');
+INSERT INTO dish_category (id, name, photo, zone_id, cabare_id)
+VALUES ('1', 'Закуски', 'snacks.jpg', '1', 1);
+INSERT INTO dish_category (id, name, photo, zone_id, cabare_id)
+VALUES ('2', 'Основные блюда', 'main.jpg', '1', 1);
+INSERT INTO dish_category (id, name, photo, zone_id, cabare_id)
+VALUES ('3', 'Салаты', 'salads.jpg', '1', 1);
+INSERT INTO dish_category (id, name, photo, zone_id, cabare_id)
+VALUES ('4', 'Напитки', 'beverages.jpg', '2', 1);
+INSERT INTO dish_category (id, name, photo, zone_id, cabare_id)
+VALUES ('5', 'Десерты', 'deserts.jpg', '1', 1);
 # dishes
 INSERT INTO dish (id, name, photo, dish_out, price, dish_category_id)
 VALUES ('1', 'Салат Весна', 'spring-salad.jpg', '5', '5000', '3');
