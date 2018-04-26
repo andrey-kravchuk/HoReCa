e6321da5470637a3cd9a60742f4974db748f39a4
andrey-kravchuk <kravchukodessa@gmail.com>
2018-04-26 21:34:38 +0300

    add unit size entity

---
+++ entities/src/main/java/cabare/entity/model/UnitSize.java
@@ -0,0 +1,104 @@
+package cabare.entity.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "unit_size")
+public class UnitSize {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", unique = true)
+  private String name;
+
+  @Column(name = "value")
+  private BigDecimal value;
+
+  @Column(name = "abbreviation")
+  private String abbreviation;
+
+  @ManyToOne
+  @JoinColumn(name = "cabare_id")
+  private Cabare cabare;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public BigDecimal getValue() {
+    return value;
+  }
+
+  public void setValue(BigDecimal value) {
+    this.value = value;
+  }
+
+  public String getAbbreviation() {
+    return abbreviation;
+  }
+
+  public void setAbbreviation(String abbreviation) {
+    this.abbreviation = abbreviation;
+  }
+
+  public Cabare getCabare() {
+    return cabare;
+  }
+
+  public void setCabare(Cabare cabare) {
+    this.cabare = cabare;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof UnitSize)) {
+      return false;
+    }
+    UnitSize unitSize = (UnitSize) o;
+    return Objects.equals(name, unitSize.name) &&
+        Objects.equals(cabare, unitSize.cabare);
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(name, cabare);
+  }
+
+  @Override
+  public String toString() {
+    return "UnitSize{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", value=" + value +
+        ", abbreviation='" + abbreviation + '\'' +
+        ", cabare=" + cabare +
+        '}';
+  }
+}
